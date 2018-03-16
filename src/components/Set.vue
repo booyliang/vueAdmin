1354f7ef0ab8d412a97794890e87331dcaa1be32
huliang <huliang1107@163.com>
2018-03-16 15:25:16 +0800

    first commit

---
+++ src/components/Set.vue
@@ -0,0 +1,36 @@
+<template>
+  <div>Set</div>
+</template>
+<script>
+export default{
+    methods: {
+      set() {
+          const set = new Set([1, 2, 3, 4, 4]);
+          console.log([...set]); // 去重
+      },
+
+      // class
+      newClass(value) {
+          class Bar {
+              doStuff() {
+                  console.log('sthff');
+              };
+              getName(value) {
+                  console.log(value)
+              }
+          }
+          let b = new Bar();
+          b.doStuff();
+          b.getName(value);
+      } 
+      
+      
+    },
+    mounted () { 
+        this.newClass('ject');
+        
+    }
+
+}
+    
+</script>
